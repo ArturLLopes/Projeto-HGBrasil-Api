7a3a7d35b6557b31543a35de8d6ca2482cd7c20b
ArturLLopes <arturllopes@gmail.com>
2026-03-30 13:23:21 -0300

    FEAT: Implementação do ResponseModel

--- api/src/main/java/br/com/projeto/hgbrasil/api/resources/PrevisaoTempoResource.java
+++ api/src/main/java/br/com/projeto/hgbrasil/api/resources/PrevisaoTempoResource.java
@@ -1,6 +1,8 @@
 package br.com.projeto.hgbrasil.api.resources;
 
+import br.com.projeto.hgbrasil.api.models.PrevisaoTempoResponseModel;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrevisaoTempoResource {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/")
-    public String test(){
-        return "teste";
+    public ResponseEntity<PrevisaoTempoResponseModel> test(){
+
+        var ret = PrevisaoTempoResponseModel.builder().nomeCidade("Rio De Janeiro").hora(16).temperatura(29).build();
+
+        return ResponseEntity.ok(ret);
     }
 }
